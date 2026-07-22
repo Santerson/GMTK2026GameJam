06afef72bcbf9204a54446f1c6917aff60ad0e90
Santerson <santacap2019@gmail.com>
2026-07-22 13:46:55 -0700

    basic shitty player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,63 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [Header("Movement")]
+    [SerializeField] float speed = 5f;
+    [SerializeField] float jumpForce = 5f;
+    [Header("Jumping")]
+    [SerializeField] Vector2 GroundedRaycastOffset = new Vector2(0, -1f);
+    Rigidbody2D refRB;
+
+
+    private void Start()
+    {
+        refRB = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawLine((Vector2)transform.position + GroundedRaycastOffset, (Vector2)transform.position + GroundedRaycastOffset + Vector2.down * 0.1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleMovement();
+    }
+
+    /// <summary>
+    /// Handles all movement on the 2d plane for the character, including jumping and moving
+    /// </summary>
+    void HandleMovement()
+    {
+        // Horizontal movement
+        Vector2 velocity = Vector2.zero;
+        if (Input.GetKey(KeyCode.D))
+        {
+            velocity += Vector2.right;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            velocity += Vector2.left;
+        }
+        refRB.linearVelocityX = velocity.x * speed;
+
+        // Jumping
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
+        {
+            refRB.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+        }
+    }
+
+    bool IsGrounded()
+    {
+        RaycastHit2D hit = Physics2D.Raycast((Vector2)transform.position + GroundedRaycastOffset, Vector2.down, 0.1f);
+        if (hit.collider != null && hit.collider.CompareTag("Ground"))
+        {
+            return true;
+        }
+        return false;
+    }
+}
