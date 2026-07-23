bf2832885fb4e05a07a2a31a25e1ea86401dc5d0
Santerson <santacap2019@gmail.com>
2026-07-23 14:00:41 -0700

    jump off the ground into swim

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -11,29 +11,41 @@ public class PlayerMovement : MonoBehaviour
         [SerializeField] float maxSpeed = 5f;
     [Tooltip("The acceleration of the player")]
         [SerializeField] float acceleration = 10f;
-    [Tooltip("The dampening of the acceleration when in mid air (0.5 is half the speed)")]
-        [SerializeField] float midAirAccelerationDampen = 0.5f;
     [Tooltip("The deceleration of the player when no input is given")]
         [SerializeField] float deceleration = 10f;
     [Tooltip("Whether or not to flip the player's sprite depending on which way they are going")]
         [SerializeField] bool flipSprite = true;
 
-    [Header("Jumping")]
+    [Header("Grounded Status")]
     [Tooltip("Where the raycast for being grounded is")]
-        [SerializeField] Vector2 LeftGroundedRaycastOffset = new Vector2(0, -1f);
+    [SerializeField] Vector2 LeftGroundedRaycastOffset = new Vector2(0, -1f);
     [Tooltip("Where the raycast for being grounded is")]
-        [SerializeField] Vector2 RightGroundedRaycastOffset = new Vector2(0, -1f);
-    [Tooltip("The max jump force of the player. An instnat jump uses half this jump height")]
-        [SerializeField] float maxJumpForce = 7f;
-    [Tooltip("The minimum jump height")]
-        [SerializeField] float minJumpForce = 2f;
-    [Tooltip("The time it takes to charge a jump (real time). Note: half of this will be used instantly on button press")]
-        [SerializeField] float jumpChargeTime = 1.5f;
-    [Tooltip("Should the player automatically jump when the jump time runs out")]
-        [SerializeField] bool AutoJump = true;
-    [Tooltip("The line renderer for the jump charge")]
-        [SerializeField] LineRenderer jumpChargeLine;
-    [SerializeField] float jumpLineMaxLength = 1.5f;
+    [SerializeField] Vector2 RightGroundedRaycastOffset = new Vector2(0, -1f);
+    //[Tooltip("The max jump force of the player. An instnat jump uses half this jump height")]
+    //    [SerializeField] float maxJumpForce = 7f;
+    //[Tooltip("The minimum jump height")]
+    //    [SerializeField] float minJumpForce = 2f;
+    //[Tooltip("The time it takes to charge a jump (real time). Note: half of this will be used instantly on button press")]
+    //    [SerializeField] float jumpChargeTime = 1.5f;
+    //[Tooltip("Should the player automatically jump when the jump time runs out")]
+    //    [SerializeField] bool AutoJump = true;
+    //[Tooltip("The line renderer for the jump charge")]
+    //    [SerializeField] LineRenderer jumpChargeLine;
+    //[SerializeField] float jumpLineMaxLength = 1.5f;
+
+    [Header("Swimming")]
+    [Tooltip("The speed the player should swim at (should be positive)")]
+        [SerializeField] float maxSwimSpeed = 2f;
+    [Tooltip("The acceleration of the player while swimming")]
+        [SerializeField] float swimAcceleration = 5f;
+    [Tooltip("The veloicty the player gets when jumping off the ground")]
+        [SerializeField] float swimImpulse = 10f;
+    [Tooltip("The time it takes to interpolate to the max swim speed from the impulse")]
+        [SerializeField] float SwimSpeedChangeTime = 0.5f;
+    [Tooltip("The dampening of the acceleration when in mid air (0.5 is half the speed)")]
+        [SerializeField] float midAirAccelerationDampen = 0.5f;
+    [Tooltip("The dampening of the deceleration when in mid air (0.5 is half the speed)")]
+        [SerializeField] float midAirDecelerationDampen = 0.5f;
 
     [Header("Glide")]
     [Tooltip("The speed the player should glide at (should be positive)")]
@@ -57,16 +69,17 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody2D refRB;
     SpriteRenderer refRenderer;
 
-    float timeSpentChargingJump = 0;
+    // float timeSpentChargingJump = 0;
+    [SerializeField] float currentSwimSpeedCap = 3f;
+    float timeSpentSwimming = 0f;
 
     public bool canMove = true;
-    Vector2 SpawnPoint = Vector2.zero;
 
     private void Start()
     {
         refRB = GetComponent<Rigidbody2D>();
         refRenderer = GetComponent<SpriteRenderer>();
-        SpawnPoint = transform.position;
+        currentSwimSpeedCap = maxSwimSpeed;
     }
 
     private void OnDrawGizmosSelected()
@@ -84,7 +97,8 @@ public class PlayerMovement : MonoBehaviour
         if (canMove)
         {
             HandleHorizontalMovement();
-            HandleVerticalMovement();
+            // HandleVerticalMovement();
+            HandleSwimLogic();
             CheckForOutOfTime();
             // Check for r key to reset
             if (Input.GetKeyDown(KeyCode.R))
@@ -128,16 +142,17 @@ public class PlayerMovement : MonoBehaviour
         {
             refRB.linearVelocity = new Vector2(Mathf.Clamp(refRB.linearVelocity.x + velocity.x * Time.deltaTime, -maxSpeed, maxSpeed), refRB.linearVelocity.y);
         }
-        else if (IsGrounded())
+        else
         {
+            float currDecel = IsGrounded() ? deceleration : deceleration * midAirDecelerationDampen;
             // Decelerate the player when no input is given
             if (refRB.linearVelocity.x > 0)
             {
-                refRB.linearVelocity = new Vector2(Mathf.Max(0, refRB.linearVelocity.x - deceleration * Time.deltaTime), refRB.linearVelocity.y);
+                refRB.linearVelocity = new Vector2(Mathf.Max(0, refRB.linearVelocity.x - currDecel * Time.deltaTime), refRB.linearVelocity.y);
             }
             else if (refRB.linearVelocity.x < 0)
             {
-                refRB.linearVelocity = new Vector2(Mathf.Min(0, refRB.linearVelocity.x + deceleration * Time.deltaTime), refRB.linearVelocity.y);
+                refRB.linearVelocity = new Vector2(Mathf.Min(0, refRB.linearVelocity.x + currDecel * Time.deltaTime), refRB.linearVelocity.y);
             }
         }
 
@@ -155,77 +170,31 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    /// <summary>
-    /// Handles all movement on the y axis for the player, including jumping and gliding
-    /// </summary>
-    void HandleVerticalMovement()
+    void HandleSwimLogic()
     {
-        // Deduct timer if holding space
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKey(KeyCode.Space) && JumpMovementTimeLeft > 0)
         {
+
+
+            // Check if grounded and give a big impulse instead of swimming if so
+            if (IsGrounded())
+            {
+                timeSpentSwimming = 0;
+                refRB.linearVelocity = new(refRB.linearVelocity.x, Mathf.Max(refRB.linearVelocityY + swimImpulse * Time.deltaTime, swimImpulse));
+            }
+            else
+            {
+                // Calculate the current swim speed cap
+                timeSpentSwimming += Time.deltaTime;
+                currentSwimSpeedCap  = Mathf.Lerp(swimImpulse, maxSwimSpeed, timeSpentSwimming / SwimSpeedChangeTime);
+                // Apply swim acceleration
+                refRB.linearVelocity = new(refRB.linearVelocity.x, Mathf.Min(refRB.linearVelocity.y + swimAcceleration * Time.deltaTime, currentSwimSpeedCap));
+            }
+            // Reduce time for jump
             JumpMovementTimeLeft -= Time.deltaTime;
         }
-
-        // If input down while grounded
-        if (Input.GetKey(KeyCode.Space) && JumpMovementTimeLeft > 0
-            && IsGrounded() && timeSpentChargingJump < jumpChargeTime)
-        {
-            HandleChargeJumpLogic();
-        }
-        // Launch the jump if input up or time runs out
-        if ((Input.GetKeyUp(KeyCode.Space) && IsGrounded() && JumpMovementTimeLeft >= 0)
-            || (timeSpentChargingJump > 0 && AutoJump && JumpMovementTimeLeft <= 0))
-        {
-            HandleJumpLogic();
-        }
-        // Reset charge if falling off a ledge
-        if (!IsGrounded())
-        {
-            timeSpentChargingJump = 0;
-            jumpChargeLine.enabled = false;
-        }
-
-        // If in air, glide
-        if (Input.GetKey(KeyCode.Space) && !IsGrounded() && refRB.linearVelocityY <= -glideFallSpeed && JumpMovementTimeLeft > 0)
-        {
-            refRB.linearVelocityY = -glideFallSpeed;
-        }
     }
 
-    /// <summary>
-    /// Handles logic for the charge jump system
-    /// </summary>
-    void HandleChargeJumpLogic()
-    {
-        // Charge the jump
-        timeSpentChargingJump += Time.deltaTime;
-        // Reduce time for jump
-        if (timeSpentChargingJump > jumpChargeTime)
-        {
-            // Reset to max charge time
-            timeSpentChargingJump = jumpChargeTime;
-        }
-       
-        // Update the line renderer for the jump
-        jumpChargeLine.enabled = true;
-        jumpChargeLine.SetPosition(1, new(0, Mathf.Lerp(0, jumpLineMaxLength, timeSpentChargingJump / jumpChargeTime)));
-    }
-
-    /// <summary>
-    /// Handles logic for jumping the player
-    /// </summary>
-    void HandleJumpLogic()
-    {
-        // Lerp the jump force based on how long the player charged the jump
-        float jumpForce = Mathf.Lerp(0, maxJumpForce, timeSpentChargingJump / jumpChargeTime);
-
-        jumpForce = Mathf.Max(jumpForce, minJumpForce); // Ensure the jump force is at least the minimum jump force
-        // Apply the force
-        refRB.linearVelocityY = jumpForce;
-        // Reset variables
-        timeSpentChargingJump = 0;
-        jumpChargeLine.enabled = false;
-    }
 
     /// <summary>
     /// Updates the time left for each movement type and updates the UI text accordingly. This is temporary
@@ -281,21 +250,11 @@ public class PlayerMovement : MonoBehaviour
     }
 
     /// <summary>
-    /// Resets the level, does NOT reset props as of now
+    /// Resets the level
     /// </summary>
     void ResetLevel()
     {
-        //// Reset the player position to the spawn point
-        //transform.position = SpawnPoint;
-        //// Reset the player velocity
-        //refRB.linearVelocity = Vector2.zero;
-        //// Reset the player sprite
-        //refRenderer.flipY = false;
-        //// TODO: Re-open the time bank menu here
-        //FindFirstObjectByType<TimeBank>().ActivateUI();
-        // Reload the current scene
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
-
     }
 
     /// <summary>
@@ -330,4 +289,77 @@ public class PlayerMovement : MonoBehaviour
         // Change the level here
         FindFirstObjectByType<GameSceneManager>().LoadLevel((int)FindFirstObjectByType<LevelGoal>().LevelIndex + 1);
     }
+    
+    // This is the old jump behavitor.
+    ///// <summary>
+    ///// Handles all movement on the y axis for the player, including jumping and gliding
+    ///// </summary>
+    //void HandleVerticalMovement()
+    //{
+    //    // Deduct timer if holding space
+    //    if (Input.GetKey(KeyCode.Space))
+    //    {
+    //        JumpMovementTimeLeft -= Time.deltaTime;
+    //    }
+
+    //    // If input down while grounded
+    //    if (Input.GetKey(KeyCode.Space) && JumpMovementTimeLeft > 0
+    //        && IsGrounded() && timeSpentChargingJump < jumpChargeTime)
+    //    {
+    //        HandleChargeJumpLogic();
+    //    }
+    //    // Launch the jump if input up or time runs out
+    //    if ((Input.GetKeyUp(KeyCode.Space) && IsGrounded() && JumpMovementTimeLeft >= 0)
+    //        || (timeSpentChargingJump > 0 && AutoJump && JumpMovementTimeLeft <= 0))
+    //    {
+    //        HandleJumpLogic();
+    //    }
+    //    // Reset charge if falling off a ledge
+    //    if (!IsGrounded())
+    //    {
+    //        timeSpentChargingJump = 0;
+    //        jumpChargeLine.enabled = false;
+    //    }
+
+    //    // If in air, glide
+    //    if (Input.GetKey(KeyCode.Space) && !IsGrounded() && refRB.linearVelocityY <= -glideFallSpeed && JumpMovementTimeLeft > 0)
+    //    {
+    //        refRB.linearVelocityY = -glideFallSpeed;
+    //    }
+    //}
+
+    ///// <summary>
+    ///// Handles logic for the charge jump system
+    ///// </summary>
+    //void HandleChargeJumpLogic()
+    //{
+    //    // Charge the jump
+    //    timeSpentChargingJump += Time.deltaTime;
+    //    // Reduce time for jump
+    //    if (timeSpentChargingJump > jumpChargeTime)
+    //    {
+    //        // Reset to max charge time
+    //        timeSpentChargingJump = jumpChargeTime;
+    //    }
+       
+    //    // Update the line renderer for the jump
+    //    jumpChargeLine.enabled = true;
+    //    jumpChargeLine.SetPosition(1, new(0, Mathf.Lerp(0, jumpLineMaxLength, timeSpentChargingJump / jumpChargeTime)));
+    //}
+
+    ///// <summary>
+    ///// Handles logic for jumping the player
+    ///// </summary>
+    //void HandleJumpLogic()
+    //{
+    //    // Lerp the jump force based on how long the player charged the jump
+    //    float jumpForce = Mathf.Lerp(0, maxJumpForce, timeSpentChargingJump / jumpChargeTime);
+
+    //    jumpForce = Mathf.Max(jumpForce, minJumpForce); // Ensure the jump force is at least the minimum jump force
+    //    // Apply the force
+    //    refRB.linearVelocityY = jumpForce;
+    //    // Reset variables
+    //    timeSpentChargingJump = 0;
+    //    jumpChargeLine.enabled = false;
+    //}
 }
