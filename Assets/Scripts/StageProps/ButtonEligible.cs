768a10c37b46e7a549d9130a1cf71966ffb91ae7
Santerson <santacap2019@gmail.com>
2026-07-22 17:37:17 -0700

    button and bux

---
+++ Assets/Scripts/StageProps/ButtonEligible.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public class ButtonEligible : MonoBehaviour
+{
+    // This script literally just exists as silent protest that unity only lets you have one tag per gameobject
+}
---
+++ Assets/Scripts/StageProps/CrateButton.cs
@@ -0,0 +1,71 @@
+using System;
+using UnityEngine;
+using UnityEngine.Events;
+using System.Collections.Generic;
+
+public class CrateButton : MonoBehaviour
+{
+    [SerializeField] bool EnableColorSwaps = false;
+    [SerializeField] GameObject[] Objects;
+    [SerializeField] UnityEvent[] EnableActions;
+    [SerializeField] UnityEvent[] DisableActions;
+
+    List<GameObject> ButtonEligibleCollisions = new List<GameObject>();
+
+    SpriteRenderer refRenderer = null;
+
+    private void Start()
+    {
+        refRenderer = GetComponent<SpriteRenderer>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ButtonEligible>() != null)
+        {
+            EnableButton();
+            ButtonEligibleCollisions.Add(collision.gameObject);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.GetComponent<ButtonEligible>() != null)
+        {
+            ButtonEligibleCollisions.Remove(collision.gameObject);
+            if (ButtonEligibleCollisions.Count == 0)
+                DisableButton();
+        }
+    }
+
+    void EnableButton()
+    {
+        if (EnableColorSwaps) refRenderer.color = Color.green;
+        // Disable all gameobjects
+        foreach (GameObject obj in Objects)
+        {
+            obj.SetActive(false);
+        }
+        // Invoke all enable actions
+        foreach (UnityEvent action in EnableActions)
+        {
+            action.Invoke();
+        }
+    }
+
+    void DisableButton()
+    {
+        if (EnableColorSwaps) refRenderer.color = Color.red;
+        // Enable all gameobjects
+        foreach (GameObject obj in Objects)
+        {
+            obj.SetActive(true);
+        }
+        // Invoke all disable actions
+        foreach (UnityEvent action in DisableActions)
+        {
+            action.Invoke();
+        }
+    }
+}
