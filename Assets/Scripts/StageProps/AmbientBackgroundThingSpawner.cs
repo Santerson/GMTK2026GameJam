92b5de54bc53529389903ab60e9f84c34c4f97b9
Santerson <santacap2019@gmail.com>
2026-07-25 14:33:08 -0700

    fixed box dropping

--- Assets/Scripts/StageProps/AmbientBackgroundThingSpawner.cs
+++ Assets/Scripts/StageProps/AmbientBackgroundThingSpawner.cs
@@ -12,10 +12,11 @@ public class AmbientBackgroundThingSpawner : MonoBehaviour
     [SerializeField] Vector2 Fishies_SpawnInterval = new Vector2(10, 15);
     [SerializeField] Vector2 Fishies_SpawnHeight = new(-5, 5);
 
-    float timeToNextSeaDragon = 0;
+    float timeToNextSeaDragon = 10;
     GameObject spawnedSeaDragon;
 
     float timeToNextFishy = 0;
+    GameObject spawnedFishy;
     // Update is called once per frame
     void Update()
     {
@@ -29,12 +30,12 @@ public class AmbientBackgroundThingSpawner : MonoBehaviour
         }
 
         // Fishies
-        if (spawnedSeaDragon == null)
-            timeToNextSeaDragon -= Time.deltaTime;
-        if (timeToNextSeaDragon < 0)
+        if (spawnedFishy == null)
+            timeToNextFishy -= Time.deltaTime;
+        if (timeToNextFishy < 0)
         {
-            SpawnSeaDragon();
-            timeToNextSeaDragon = Random.Range(SeaDragon_SpawnInterval.x, SeaDragon_SpawnInterval.y);
+            SpawnFishy();
+            timeToNextFishy = Random.Range(Fishies_SpawnInterval.x, Fishies_SpawnInterval.y);
         }
     }
 
@@ -42,5 +43,11 @@ public class AmbientBackgroundThingSpawner : MonoBehaviour
     {
         spawnedSeaDragon = Instantiate(RightSeaDragonPrefab, new(transform.position.x, Random.Range(SeaDragon_SpawnHeight.x, SeaDragon_SpawnHeight.y)), Quaternion.identity);
         spawnedSeaDragon.transform.SetParent(transform, false);
+    }
+
+    void SpawnFishy()
+    {
+        spawnedFishy = Instantiate(FishiesPrefab, new(transform.position.x, Random.Range(Fishies_SpawnHeight.x, Fishies_SpawnHeight.y)), Quaternion.identity);
+        spawnedFishy.transform.SetParent(transform, false);
     }
 }
