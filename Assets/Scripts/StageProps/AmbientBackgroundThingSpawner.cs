eac0bab64d3e1f88be455f248d206d1b0f40dceb
Santerson <santacap2019@gmail.com>
2026-07-25 14:15:53 -0700

    bammo

--- Assets/Scripts/StageProps/AmbientBackgroundThingSpawner.cs
+++ Assets/Scripts/StageProps/AmbientBackgroundThingSpawner.cs
@@ -7,13 +7,29 @@ public class AmbientBackgroundThingSpawner : MonoBehaviour
     [SerializeField] Vector2 SeaDragon_SpawnInterval = new(20, 30);
     [SerializeField] Vector2 SeaDragon_SpawnHeight = new(-5, 5);
 
+    [Header("Fishies")]
+    [SerializeField] GameObject FishiesPrefab;
+    [SerializeField] Vector2 Fishies_SpawnInterval = new Vector2(10, 15);
+    [SerializeField] Vector2 Fishies_SpawnHeight = new(-5, 5);
+
     float timeToNextSeaDragon = 0;
     GameObject spawnedSeaDragon;
+
+    float timeToNextFishy = 0;
     // Update is called once per frame
     void Update()
     {
         // Sea dragons
         if (spawnedSeaDragon == null) 
+            timeToNextSeaDragon -= Time.deltaTime;
+        if (timeToNextSeaDragon < 0)
+        {
+            SpawnSeaDragon();
+            timeToNextSeaDragon = Random.Range(SeaDragon_SpawnInterval.x, SeaDragon_SpawnInterval.y);
+        }
+
+        // Fishies
+        if (spawnedSeaDragon == null)
             timeToNextSeaDragon -= Time.deltaTime;
         if (timeToNextSeaDragon < 0)
         {
